bc964ad4c54a553f896f4643275d06acb7e2b73e
Shaliko Salimov <90727897+ksilisk@users.noreply.github.com>
2025-11-16 21:17:35 +0300

    added the Interceptor interface for preprocessing updates (#46)

--- telegram-bot-spring-boot-autoconfigure/src/main/java/io/ksilisk/telegrambot/autoconfigure/config/dispatch/DispatcherConfiguration.java
+++ telegram-bot-spring-boot-autoconfigure/src/main/java/io/ksilisk/telegrambot/autoconfigure/config/dispatch/DispatcherConfiguration.java
@@ -2,7 +2,6 @@ package io.ksilisk.telegrambot.autoconfigure.config.dispatch;
 
 import io.ksilisk.telegrambot.core.dispatcher.SimpleUpdateDispatcher;
 import io.ksilisk.telegrambot.core.dispatcher.UpdateDispatcher;
-import io.ksilisk.telegrambot.core.handler.exception.CompositeExceptionHandler;
 import io.ksilisk.telegrambot.core.router.UpdateRouter;
 import io.ksilisk.telegrambot.core.strategy.CompositeNoMatchStrategy;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -17,8 +16,7 @@ public class DispatcherConfiguration {
     @Bean
     @ConditionalOnMissingBean(UpdateDispatcher.class)
     public UpdateDispatcher updateDispatcher(List<UpdateRouter> updateRouters,
-                                             CompositeExceptionHandler compositeExceptionHandler,
                                              CompositeNoMatchStrategy compositeNoMatchStrategy) {
-        return new SimpleUpdateDispatcher(updateRouters, compositeExceptionHandler, compositeNoMatchStrategy);
+        return new SimpleUpdateDispatcher(updateRouters, compositeNoMatchStrategy);
     }
 }
