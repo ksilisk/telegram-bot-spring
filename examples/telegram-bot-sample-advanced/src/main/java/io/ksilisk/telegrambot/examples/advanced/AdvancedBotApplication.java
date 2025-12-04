98d2fdffbbe99530f367d65dbbf4472f14755453
Charan Raju Manda <164352761+charanraju834@users.noreply.github.com>
2025-12-04 23:51:55 +0530

    feat(examples): add main application class for advanced bot

---
+++ examples/telegram-bot-sample-advanced/src/main/java/io/ksilisk/telegrambot/examples/advanced/AdvancedBotApplication.java
@@ -0,0 +1,12 @@
+package io.ksilisk.telegrambot.examples.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedBotApplication.class, args);
+    }
+}
